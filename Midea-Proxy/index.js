c6ff38078b72e4eb3646b91bfad953184970a159
mirllan <145820832+mirllan@users.noreply.github.com>
2025-10-30 11:08:55 -0500

    correccion

--- Midea-Proxy/index.js
+++ Midea-Proxy/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fetch = require("node-fetch");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const AUTH_HEADERS = {
     Authorization: "Token 9b7661d9292aab2c339b95bf251063791c2a62ff",
     "Content-Type": "application/json",
